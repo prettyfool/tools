91a51a2f178347f2daad30be2789cb583ab34463
prettyfool <majunfeng722@126.com>
2017-08-18 19:39:38 +0800

    去掉临时目录 temp 改为默认目录

--- verifyChannel/verifyChannel.py
+++ verifyChannel/verifyChannel.py
@@ -24,110 +24,118 @@ def tugeleLinks():
     urls = [os.path.join(url, filename) for filename in filelist]
     return urls
 
-def downAPK(url,out='./'):
-	"""
+
+def downAPK(url, out='./'):
+    """
 	@des 下载文件
 	@url 下载链接
 	"""
-	try:
-		r = requests.get(url, stream=True, verify=False)
-	except requests.exceptions.HTTPError, e:
-		print '%s is Error\n%s' % (url, e)
-	else:
-		name = os.path.basename(url)
-		file_name = os.path.join(out, name)
-		CHUNK_SIZE = 16 * 1024
-		with open(file_name, 'wb') as f:
-			logger.debug('success download %s'%name)
-			for chunk in r.iter_content(CHUNK_SIZE):
-				f.write(chunk)
+    try:
+        r = requests.get(url, stream=True, verify=False)
+    except requests.exceptions.HTTPError, e:
+        print '%s is Error\n%s' % (url, e)
+    else:
+        name = os.path.basename(url)
+        file_name = os.path.join(out, name)
+        CHUNK_SIZE = 16 * 1024
+        with open(file_name, 'wb') as f:
+            logger.debug('success download %s' % name)
+            for chunk in r.iter_content(CHUNK_SIZE):
+                f.write(chunk)
 
 
 # 2.解析安装包获取Manifest文件中的渠道号和pushTag
 
 def getManifest(path):
+    """
+	@des 解apk包
 	"""
-	@des 解apk包到temp目录
-	"""
-	cmd = 'java -jar apktool.jar d %s -o temp > nul'%path
-	logger.info('parsing package...')
-	logger.info(os.path.basename(path))
-	os.system(cmd)
+    cmd = 'java -jar apktool.jar d %s > nul' % path
+    logger.info('parsing package...')
+    logger.info(os.path.basename(path))
+    os.system(cmd)
+
 
 def getChannel(xml):
-	"""
+    """
 	@des 解析Manifest.xml获取channelID,pushTag
 	@return [channelID,pushTag]
 	"""
-	info = {}
-	tree = etree.parse(xml)
-	meta = tree.xpath('//meta-data')
-	return [me.values()[1] for me in meta][0:2]
+    info = {}
+    tree = etree.parse(xml)
+    meta = tree.xpath('//meta-data')
+    return [me.values()[1] for me in meta][0:2]
+
+
+def getTemp():
+    dirs = os.listdir('./')
+    for i in dirs:
+        if i.startswith('SG') and os.path.isdir(i):
+            return i
 
 
 # 3.验证渠道号和pushTag
 def verify(path):
-	"""
+    """
 	@des 验证渠道号和pushtag
 	@path 安装包路径
 	"""
-	getManifest(path)
-	channel,pushtag = getChannel('temp\AndroidManifest.xml')
-	if pushtag =='online' and channel in path:
-		result[channel] = 'pass'
-	else:
-		result[channel] = 'fail'
-	logger.info('Verifying ...')
+    getManifest(path)
+    temp = getTemp()
+    channel, pushtag = getChannel(os.path.join(temp, 'AndroidManifest.xml'))
+    if pushtag == 'online' and channel in path:
+        result[channel] = 'pass'
+    else:
+        result[channel] = 'fail'
+    logger.info('Verifying ...')
 
 
 # 4.清理文件
 
 def delfile(src):
-	shutil.rmtree(src)
+    """
+	@desc 删除非空目录src
+	@src 非空目录全路径
+	"""
+    shutil.rmtree(src)
 
-#abandon
-def delfile1(src):
-	if os.path.isfile(src):
-		os.remove(src)
-	if os.path.isdir(src):
-		fi = os.listdir(src)
-		if len(fi):
-			for i in fi:
-				p = os.path.join(src,i)
-				delfile(p)
-		else:
-			os.rmdir(src)
 
 def clear():
-	src = r'.\temp'
-	logger.info('clear directory temp...')
-	delfile(src)
+    """
+	@desc 清理目录
+	"""
+    temp = getTemp()
+    logger.info('clear directory ')
+    delfile(temp)
+
 
 # 5.生成报告
 def report():
-	logger.info('get report file...')
-	with open('result.txt','w') as f:
-		for item in result:
-			f.write(item)
-			f.write('\n')
+    """
+	@desc 把测试通过的渠道号写入result.txt
+	"""
+    logger.info('get report file...')
+    with open('result.txt', 'w') as f:
+        for item in result:
+            f.write(item)
+            f.write('\n')
 
 
 if __name__ == '__main__':
-	#下载所有安装包
-	if not os.path.exists('package'):
-		logger.info('package is not exist ,creating it...')
-		os.mkdir('package')
-	apklist = os.listdir('package')
-	if not len(apklist):
-		links = tugeleLinks()
-		logger.info('downloading ...')
-		for url in links:
-			downAPK(url,out='package')
-		apklist = os.listdir('package')
+    # 下载所有安装包
+    if not os.path.exists('package'):
+        logger.info('package is not exist ,creating it...')
+        os.mkdir('package')
+    apklist = os.listdir('package')
+    if not len(apklist):
+        links = tugeleLinks()
+        logger.info('downloading ...')
+        for url in links:
+            downAPK(url, out='package')
+        apklist = os.listdir('package')
 
-	for apk in apklist:
-		path = os.path.join(os.curdir,'package',apk)
-		verify(path)
-		clear()
-	report()
-
+    for apk in apklist:
+        path = os.path.join(os.curdir, 'package', apk)
+        verify(path)
+        clear()
+    report()
