7765053196143c11708d9d5c50f05a78baf47a9f
prettyfool <majunfeng722@126.com>
2017-12-11 21:40:03 +0800

    简化一键卸载所有非保留的app方法

--- uninstall_all_packages.py
+++ uninstall_all_packages.py
@@ -1,25 +1,13 @@
 # -*- coding:utf-8 -*-
-import os
+import os, re
 
-# 打算保留的应用列表
 package_keep = [
+    # 打算保留的应用列表
     'com.sohu.inputmethod.sogou',
     'com.tencent.mobileqq',
     'com.xp.tugele'
 ]
-apps = os.popen('adb shell pm list packages -3').readlines()
-installed_app = [app.split(':')[1].rstrip() for app in apps]
-
-if len(installed_app) > 0:
-    for app in installed_app:
-        if app not in package_keep:
-            cmd = 'adb uninstall ' + app + ' >nul'
-            res = os.system(cmd)
-            if res == 0:
-                print '[success] %s uninstalled' % app
-            else:
-                print '[fail] %s uninstalled' % app
-    else:
-        print 'uninstalled all app'
-else:
-    print 'no packages from 3'
+# 卸载不在package_keep 中的所有app
+[os.popen('adb uninstall %s' % app) for app in
+ (re.search('package:(\S+)', line).group(1) for line in os.popen("adb shell pm list packages -3") if
+  line and line.startswith('package')) if app not in package_keep]
