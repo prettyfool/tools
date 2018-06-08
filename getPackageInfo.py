4a69bbdea105ca231fdebddb6bb014c36aa96c78
prettyfool <majunfeng722@126.com>
2018-06-08 21:55:57 +0800

    优化更新

--- getPackageInfo.py
+++ getPackageInfo.py
@@ -1,9 +1,12 @@
 # -*- coding:utf-8 -*-
-import os, re
+import os
+import re
 
 
-def getLaunchActivity(apkpath):
-    # 获取app 启动 activity
+def get_launch_activity(apkpath):
+    """
+    : 获取app 启动 activity
+    """
     cmd = 'aapt d badging %s | findstr launch' % apkpath
     std = os.popen(cmd)
     for line in std:
@@ -12,9 +15,11 @@ def getLaunchActivity(apkpath):
             return launch_activity
 
 
-def getAPPInfo(apkpath):
-    # 获取apk 的包名，版本号，版本名称
-    # return {packageName: '%s', versionCode: '%s', versionName: '%s'}
+def get_package_info(apkpath):
+    """
+    : 获取apk 的包名，版本号，版本名称
+    : return {packageName: '%s', versionCode: '%s', versionName: '%s'}
+    """
     cmd = 'aapt d badging %s | findstr package' % apkpath
     std = os.popen(cmd)
     info = {}
@@ -26,28 +31,38 @@ def getAPPInfo(apkpath):
     return info
 
 
-def getPackageName(apkpath):
-    # 获取apk 的包名
-    return getAPPInfo(apkpath)['packageName']
+def get_package_name(apkpath):
+    """
+    : 获取apk 的包名
+    """
+    return get_package_info(apkpath)['packageName']
 
 
-def decodeAPK(apkpath):
-    # 反编译apk到当前目录
+def unpack_apk(apkpath):
+    """
+    : 反编译apk到当前目录
+    """
     cmd = 'java -jar apktool.jar d %s' % apkpath
     os.popen(cmd)
 
 
-def getManifest(apkpath):
-    decodeAPK(apkpath)
+def get_manifest(apkpath):
+    """
+    : 获取AndroidManifest.xml 的路径
+    """
     filedir = os.path.join(os.curdir, os.path.splitext(os.path.basename(apkpath))[0])
-    if os.path.isdir(filedir):
-        manifest = os.path.join(filedir, 'AndroidManifest.xml')
-        return os.path.abspath(manifest)
+    if not os.path.exists(filedir):
+        unpack_apk(apkpath)
+        print('unpacking...')
+
+    manifest = os.path.join(filedir, 'AndroidManifest.xml')
+    return os.path.realpath(manifest)
 
 
 if __name__ == '__main__':
-    apk = r'E:\test\SGTugele_v4.0.0_Build_967_20171207_test_tugele_debug.apk'
-    # print getLaunchActivity(apk)
-    # print getPackageName(apk)
-    # print getAPPInfo(apk)
-    print(getLaunchActivity(apk))
+    apk = r'E:\package\PKG_tugele\SGTugele_v4.3.0_Build_1132_20180416_test_tugele_debug.apk'
+
+    print(get_launch_activity(apk))
+    print(get_package_name(apk))
+    print(get_package_info(apk))
+    print(get_manifest(apk))
